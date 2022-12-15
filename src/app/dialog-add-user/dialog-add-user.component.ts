1e7706dea9d319cd8cfc1fbb6db01de488aa60c1
Herlina Pfeiffer <pfeiffer.herlina@gmx.net>
2022-12-15 22:13:41 +0100

    first commit

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/models/user.class';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  templateUrl: './dialog-add-user.component.html',
+  styleUrls: ['./dialog-add-user.component.scss'],
+})
+export class DialogAddUserComponent implements OnInit {
+  user = new User();
+  
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onNoClick() {}
+
+  saveUser() {
+    console.log('Current user is', this.user);
+  }
+}
