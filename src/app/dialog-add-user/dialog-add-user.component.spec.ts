e05b31f2666c02c03f67ea024272734507f5f528
Herlina Pfeiffer <pfeiffer.herlina@gmx.net>
2022-12-18 18:20:58 +0100

    ng test with Karma done

--- src/app/dialog-add-user/dialog-add-user.component.spec.ts
+++ src/app/dialog-add-user/dialog-add-user.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { environment } from 'src/environments/environment';
 
 import { DialogAddUserComponent } from './dialog-add-user.component';
 
@@ -8,7 +12,18 @@ describe('DialogAddUserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        MatDialogModule,
+      ],
       declarations: [DialogAddUserComponent],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DialogAddUserComponent);
--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -32,14 +32,16 @@ export class UserDetailComponent implements OnInit {
   }
 
   getUser() {
-    this.firestore
-      .collection('users')
-      .doc(this.userId)
-      .valueChanges()
-      .subscribe((user: any) => {
-        this.user = new User(user);
-        console.log('Retrieve user', this.user);
-      });
+    if (this.userId) {
+      this.firestore
+        .collection('users')
+        .doc(this.userId)
+        .valueChanges()
+        .subscribe((user: any) => {
+          this.user = new User(user);
+          console.log('Retrieve user', this.user);
+        });
+    }
   }
 
   editUserDetail() {
